4c64e7ee9d694315559f3341ef1e35181a891525
Jonathan Aranguri <arangurim@gmail.com>
2024-10-23 19:32:01 -0300

    feat: Improve design and behaviour

--- app/src/main/java/com/joniaranguri/garbageguru/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/joniaranguri/garbageguru/ui/camera/CameraActivity.kt
@@ -11,7 +11,8 @@ import android.os.Bundle
 import android.view.TextureView
 import android.view.TextureView.SurfaceTextureListener
 import android.view.View
-import android.widget.ImageButton
+import android.widget.FrameLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,12 +21,26 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.button.MaterialButton
 import com.joniaranguri.garbageguru.R
+import com.joniaranguri.garbageguru.domain.MaterialDetails
 import com.joniaranguri.garbageguru.domain.Photo
+import com.joniaranguri.garbageguru.ui.common.LoadingView
 import com.joniaranguri.garbageguru.ui.recommendation.RecommendationActivity
+import kotlinx.coroutines.launch
+
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var cameraViewModel: CameraViewModel
+    private lateinit var loadingView: LoadingView
+    private lateinit var recyclingRecommendationButton: MaterialButton
+    private lateinit var materialResultsBottomSheet: FrameLayout
+    private lateinit var materialTitleTextView: TextView
+    private lateinit var savedCo2TextView: TextView
+    private lateinit var materialTypeTextView: TextView
+    private lateinit var rewardTextView: TextView
     private var textureView: TextureView? = null
 
     @SuppressLint("SourceLockedOrientationActivity")
@@ -52,15 +67,64 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun configureViews() {
-        val takePhotoButton = findViewById<ImageButton>(R.id.take_photo_button)
+        val takePhotoButton = findViewById<MaterialButton>(R.id.take_photo_button)
+        loadingView = findViewById(R.id.loading_view)
         textureView = findViewById(R.id.camera_preview)
-        takePhotoButton.setOnClickListener { cameraViewModel.capturePhoto() }
+        recyclingRecommendationButton = findViewById(R.id.get_recommendation_button)
+        materialResultsBottomSheet = findViewById(R.id.material_results_bottom_sheet)
+        materialTitleTextView = findViewById(R.id.material_title_textview)
+        savedCo2TextView = findViewById(R.id.saved_co2_textview)
+        materialTypeTextView = findViewById(R.id.material_type_textview)
+        rewardTextView = findViewById(R.id.reward_textview)
+
+        collapseBottomSheet()
+
+        takePhotoButton.setOnClickListener {
+            cameraViewModel.capturePhoto()
+        }
 
         cameraViewModel.photoLiveData.observe(this) { photo ->
             photo?.localUri?.let {
-                launchRecommendationActivity(photo)
+                lifecycleScope.launch {
+                    loadingView.show()
+                    cameraViewModel.getMaterialDetailsFromServer(it)
+                }
             }
         }
+
+        cameraViewModel.materialDetailsLiveData.observe(this) { materialDetails ->
+            materialDetails?.let {
+                showMaterialDetailsBottomSheet(materialDetails)
+            }
+        }
+    }
+
+    private fun showMaterialDetailsBottomSheet(materialDetails: MaterialDetails) {
+        setUpMaterialDetailsViews(materialDetails)
+        recyclingRecommendationButton.setOnClickListener {
+            collapseBottomSheet()
+            launchRecommendationActivity(Photo(""))
+        }
+        loadingView.hide()
+        expandBottomSheet()
+    }
+
+    private fun expandBottomSheet() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(materialResultsBottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun collapseBottomSheet() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(materialResultsBottomSheet)
+        bottomSheetBehavior.isHideable = true
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    private fun setUpMaterialDetailsViews(materialDetails: MaterialDetails) {
+        materialTypeTextView.text = materialDetails.name
+        savedCo2TextView.text = materialDetails.savedCO2
+        materialTypeTextView.text = materialDetails.materialType
+        rewardTextView.text = materialDetails.reward.toString()
     }
 
     private fun launchRecommendationActivity(photo: Photo) {
--- app/src/main/java/com/joniaranguri/garbageguru/ui/camera/CameraViewModel.kt
+++ app/src/main/java/com/joniaranguri/garbageguru/ui/camera/CameraViewModel.kt
@@ -9,7 +9,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.joniaranguri.garbageguru.controllers.CameraController
+import com.joniaranguri.garbageguru.domain.MaterialDetails
 import com.joniaranguri.garbageguru.domain.Photo
+import kotlinx.coroutines.delay
 
 /**
  * ViewModel for managing camera operations and photo capturing.
@@ -24,6 +26,9 @@ class CameraViewModel(private val cameraController: CameraController) : ViewMode
             }
             _photoLiveData.postValue(Photo(photoUri))
         }
+
+    private val _materialDetailsLiveData: MutableLiveData<MaterialDetails> = MutableLiveData<MaterialDetails>()
+    var materialDetailsLiveData: LiveData<MaterialDetails> = _materialDetailsLiveData
 
     init {
         observePhotoPath()
@@ -67,6 +72,19 @@ class CameraViewModel(private val cameraController: CameraController) : ViewMode
         }
     }
 
+    suspend fun getMaterialDetailsFromServer(photoUri: String){
+        delay(3000)
+        // TODO: Implement this with real call to the server
+        val mockedDetails = MaterialDetails(
+            name = "Botella de plástico",
+            materialType = "Plastico",
+            reward = 3,
+            savedCO2 = "6g"
+        )
+        _materialDetailsLiveData.postValue(mockedDetails)
+        return
+    }
+
     /**
      * Closes the camera.
      */
@@ -79,11 +97,11 @@ class CameraViewModel(private val cameraController: CameraController) : ViewMode
      */
     override fun onCleared() {
         super.onCleared()
-        cameraController.photoPathLiveData.removeObserver(photoPathObserver)
+        cameraController.getPhotoPathLiveData().removeObserver(photoPathObserver)
     }
 
     private fun observePhotoPath() {
-        cameraController.photoPathLiveData.observeForever(photoPathObserver)
+        cameraController.getPhotoPathLiveData().observeForever(photoPathObserver)
     }
 
     companion object {
